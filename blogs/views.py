431d1e1295ddf0aa928bbc98cabeb8f86c23f281
Abhishek.R <abhishekR3914@gmail.com>
2026-03-07 20:08:43 +0530

    Initial commit - BlogNova Django blog project

---
+++ blogs/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import redirect, render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from .models import Blog,Category,Comment
+from django.db.models import Q
+# Create your views here.
+def posts_by_category(request,category_id):
+    posts = Blog.objects.filter(status="Publish",category_id = category_id)
+    category = get_object_or_404(Category,id=category_id)
+    context = {
+        'posts' : posts,
+        'category': category,
+    }
+    return render(request,'posts_by_category.html',context)
+
+def blogs(request,slug):
+    single_blog = get_object_or_404(Blog,slug=slug,status = 'Publish')
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+    comments = Comment.objects.filter(blog = single_blog)
+    comment_count = comments.count()
+    context = {
+        'single_blog' : single_blog,
+        'comments' : comments,
+        'comment_count' : comment_count,
+    }
+    return render(request,'blogs.html',context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword),status = 'Publish')
+    context = {
+        'blogs' : blogs,
+        'keyword' : keyword
+    }
+    return render(request,'search.html',context)
---
+++ dashboards/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.dashboard,name='dashboard'),
+    path('categories/',views.categories,name='categories'),
+    path('categories/add/',views.add_category,name='add_category'),
+    path('categories/edit/<int:pk>/',views.edit_category,name='edit_category'),
+    path('categories/delete/<int:pk>/',views.delete_category,name='delete_category'),
+    path('posts/',views.posts,name='posts'),
+    path('posts/add/',views.add_posts,name='add_posts'),
+    path('posts/edit/<int:pk>/',views.edit_posts,name='edit_posts'),
+    path('posts/delete/<int:pk>/',views.delete_posts,name='delete_posts'),
+    path('users/',views.users,name='users'),
+    path('users/add_user/',views.add_user,name='add_user'),
+    path('users/edit_user/<int:pk>/',views.edit_user,name='edit_user'),
+    path('users/delete_user/<int:pk>/',views.delete_user,name='delete_user'),
+]
